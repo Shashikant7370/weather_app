98e27ccd7816daa091ee96452afbb8131821866b
shashikant kumar <sashikantkumarbth00@gmail.com>
2024-12-25 16:39:31 +0530

    some changes

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import {useWeather} from "../context/weather"
 const Location = () => {
   const state = useWeather();
-
+  console.log(state)
   return (
     <div className="flex justify-center items-center flex-col">
-      <div>
+      <div className="flex items-center justify-center">
         <h1 className="text-5xl text-white font-semibold">
           {state?.location?.current?.temp_c}℃
+          <p className="inline text-[16px] ml-3">
+            {state?.location?.current?.condition?.text}
+          </p>
         </h1>
       </div>
       <h2 className="text-2xl mt-2 text-white">
         {state?.location?.location?.region} ,
         {state?.location?.location?.country}
       </h2>
+      <h3 className="text-2xl mt-2 text-purple-300">
+        {state?.location?.location?.tz_id}
+      </h3>
     </div>
   );
 }
